85b9d0024801afc9f131a06636ab9f3cd771b071
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-08-23 00:29:42 -0300

    Home
    Home page configured and styled components install!

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+ Container,
+ Title
+} from './styles';
+
+export function Home(){
+return (
+  <Container> 
+      <Title>Welcome to the marvel app!</Title>
+  </Container>
+  );
+}
