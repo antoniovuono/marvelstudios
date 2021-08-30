2b202e194d9019d156700567defa4e68612b2e67
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-08-30 00:26:46 -0300

    Spider Man Screen
    Almost finalized!

--- src/screens/Heros/SpiderMan/index.tsx
+++ src/screens/Heros/SpiderMan/index.tsx
@@ -8,6 +8,12 @@ import WeightIcon from '../../../assets/icons/weight.svg';
 import HeightIcon from '../../../assets/icons/height.svg';
 import UniverseIcon from '../../../assets/icons/universe.svg';
 
+import StrengthBars from '../../../assets/images/strengthbars.svg';
+import IntelligenceBars from '../../../assets/images/intelligencebars.svg';
+import AgilityBars from '../../../assets/images/agilitybars.svg';
+import EnduranceBars from '../../../assets/images/endurancebars.svg';
+import VelocityBars from '../../../assets/images/velocitybars.svg';
+
 import {
  Container,
  DetailsContent,
@@ -15,6 +21,15 @@ import {
  AlterEgo,
  Name,
  CaracteristicsContent,
+ BiographyContent,
+ Description,
+ Description2,
+ SkillTitle,
+ SkillsContent,
+ SkillDetails,
+ Title,
+ ImgView,
+ TitleView,
 
 } from './styles';
 
@@ -49,9 +64,82 @@ return (
 
        </CaracteristicsContent>
 
+       <BiographyContent>
+         <Description>
+         Em Forest Hills, Queens, Nova York, o estudante de ensino médio, Peter Parker, é um cientista orfão que vive com seu tio Ben e tia May. Ele é mordido por uma aranha radioativa em uma exposição científica e adquire a agilidade e a força proporcional de um aracnídeo. Junto com a super força, Parker ganha a capacidade de andar nas paredes e tetos.
+         </Description>
+
+         <Description2>
+         Através de sua habilidade nativa para a ciência, ele desenvolve um aparelho que o permitir lançar teias artificiais. Inicialmente buscando capitalizar suas novas habilidades, Parker cria um traje e, como Homem Aranha, torna-se uma estrela de televisão.
+         </Description2>
+       </BiographyContent>
+
+
+       <SkillsContent>
+          
+            <SkillTitle>Habilidades</SkillTitle>
+            <SkillDetails>
+
+                <TitleView>
+                <Title>Força</Title>
+                </TitleView>
+
+                <ImgView>
+                <StrengthBars  />
+                </ImgView>
+            </SkillDetails>
+
+          
+            <SkillDetails>
+
+                <TitleView>
+                <Title>Inteligência</Title>
+                </TitleView>
+
+                <ImgView>
+                <IntelligenceBars  />
+                </ImgView>
+            </SkillDetails>
+
+            <SkillDetails>
+
+                <TitleView>
+                <Title>Agilidade</Title>
+                </TitleView>
+
+                <ImgView>
+                <AgilityBars  />
+                </ImgView>
+            </SkillDetails>
+
+            <SkillDetails>
+
+                <TitleView>
+                <Title>Resistência</Title>
+                </TitleView>
+
+                <ImgView>
+                <EnduranceBars  />
+                </ImgView>
+            </SkillDetails>
+
+              <SkillDetails>
+
+                <TitleView>
+                <Title>Velocidade</Title>
+                </TitleView>
+
+                <ImgView>
+                <VelocityBars  />
+                </ImgView>
+            </SkillDetails>
+
+
+       </SkillsContent>
 
      </DetailsContent>
 
+  
   </Container>
   </>
   );
--- src/screens/Heros/SpiderMan/styles.ts
+++ src/screens/Heros/SpiderMan/styles.ts
@@ -28,7 +28,7 @@ export const ArrowIcon = styled(Feather)`
 
 
 export const DetailsContent = styled.ScrollView`
-
+  
    width: 100%;
    margin-top: ${RFValue(195)}px;
 
@@ -73,12 +73,95 @@ export const CaracteristicsContent = styled.View`
    padding: 24px 24px;
 
    background-color: ${({theme}) => theme.colors.secondary_background};
-   opacity: 0.7;
+   opacity: 0.8;
 
-   margin-top: 59px;
+   margin-top: 49px;
 `;
 
 
+export const BiographyContent = styled.View`
 
+   width: 100%;
+   padding: 20px 24px;
+
+   background-color: ${({theme}) => theme.colors.secondary_background};
+   opacity: 0.8;
+  
+
+`;
+
+export const Description = styled.Text`
+
+   color:${({theme}) => theme.colors.primary_background};
+   font-size: ${RFValue(14)}px;
+   font-family: 'gilroy-medium';
+
+`;
+
+export const Description2 = styled.Text`
+
+   color:${({theme}) => theme.colors.primary_background};
+   margin-top: 18px;
+   font-size: ${RFValue(14)}px;
+   font-family: 'gilroy-medium';
+
+
+`;
+
+export const SkillsContent = styled.View`
+
+   width: 100%;
+   height: 236px;
+
+   background-color: ${({theme}) => theme.colors.secondary_background};
+   opacity: 0.8;
+
+   padding: 10px 24px;
+  
+`;
+
+export const SkillTitle = styled.Text`
+
+
+   color:${({theme}) => theme.colors.primary_background};   
+
+   font-family: 'gilroy-bold';
+   font-size: ${RFValue(18)}px;
+
+
+`;
+
+
+export const SkillDetails = styled.View`
+
+   flex-direction: row;
+   align-items: center;
+
+   margin-top: 20px;
+   
+`;
+
+export const TitleView = styled.View`
+
+
+   width: 30%;
+   
+
+`;
+
+
+export const Title = styled.Text`
+
+
+   color:${({theme}) => theme.colors.primary_background};   
+   font-size: ${RFValue(12)}px;
+
+`;
+
+export const ImgView = styled.View`
+
+   width: 70%;
+
+`;
 
 
